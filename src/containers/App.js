431798dd55ee4e26b9320828f45f74a70a535138
sushmullur <mullur.sushruth@gmail.com>
2024-05-30 22:16:19 -0700

    Added redux for state management

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,42 +1,43 @@
 import React, { useEffect, useState } from 'react';
-
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
+import { setSearchField } from '../actions';
 
+const mapStateToProps = state => ({
+  searchField: state.searchRobots.searchField
+});
 
-function App () {
+const mapDispatchToProps = dispatch => ({
+  onSearchChange: event => dispatch(setSearchField(event.target.value))
+});
+
+function App({ searchField, onSearchChange }) {
   const [robots, setRobots] = useState([]);
-  const [searchfield, setSearchfield] = useState('');
-  const [count, setCount] = useState(0);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response=> response.json())
-      .then(users => {setRobots(users)});
-  }, [])
+      .then(response => response.json())
+      .then(users => setRobots(users));
+  }, []);
 
-  const onSearchChange = (event) => {
-    setSearchfield(event.target.value)
-  }
+  const filteredRobots = robots.filter(robot =>
+    robot.name.toLowerCase().includes(searchField.toLowerCase())
+  );
 
-  const filteredRobots = robots.filter(robot =>{
-    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-  })
-
-  return !robots.length ?
-    <h1>Loading</h1> :
-    (
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <button onClick={() => setCount(count+1)}>Click Me!</button>
-        <SearchBox searchChange={onSearchChange}/>
-        <Scroll>
-          <CardList robots={filteredRobots} />
-        </Scroll>
-      </div>
-    );
+  return !robots.length ? (
+    <h1>Loading</h1>
+  ) : (
+    <div className='tc'>
+      <h1 className='f1'>RoboFriends</h1>
+      <SearchBox searchChange={onSearchChange} />
+      <Scroll>
+        <CardList robots={filteredRobots} />
+      </Scroll>
+    </div>
+  );
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import React from 'react';
-// import ReactDOM from 'react-dom'; The new way to import createRoot:
-import { createRoot } from "react-dom/client";
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { createRoot } from 'react-dom/client';
+import { createLogger } from 'redux-logger';
+import { thunk } from 'redux-thunk';
+import { searchRobots } from './reducers'; 
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
+const logger = createLogger();
+const store = configureStore({
+  reducer: {
+    searchRobots: searchRobots
+  },
 
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk, logger)
+});
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
 
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+registerServiceWorker();
